f51182faadb4ff9dbc7f1def7ccc1c91b8633aab
Chance Payne <chance@lambdaschool.com>
2019-04-11 16:43:32 -0600

    clean up unnecessary comments and unused code

--- app/src/main/java/com/lambdaschool/movingspinner/MovingSpinner.java
+++ app/src/main/java/com/lambdaschool/movingspinner/MovingSpinner.java
@@ -2,13 +2,9 @@ package com.lambdaschool.movingspinner;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
-import android.graphics.Rect;
-import android.graphics.RectF;
-import android.graphics.Region;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
@@ -19,16 +15,10 @@ import java.util.ArrayList;
 public class MovingSpinner extends View {
     Paint  paint;
     Path   path;
-    Region region;
-//    Rect rect;
 
     Shape shape;
 
-//    float x = 0, y = 0;
     float touchX, touchY;
-//    float rotation = 0;
-
-    float rectStartX, rectEndX, rectStartY, rectEndY;
 
     boolean move;
 
@@ -56,15 +46,13 @@ public class MovingSpinner extends View {
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(getResources().getColor(android.R.color.holo_red_dark));
 
-//        rect = new Rect();
         path = new Path();
-        region = new Region();
-        final ArrayList<Point> verticies = new ArrayList<>();
+        /*final ArrayList<Point> verticies = new ArrayList<>();
         verticies.add(new Point(100, 600));
         verticies.add(new Point(600, 600));
         verticies.add(new Point(600, 100));
         verticies.add(new Point(100, 100));
-        shape = new Shape(verticies, paint);
+        shape = new Shape(verticies, paint)*/;
     }
 
     @Override
@@ -73,19 +61,7 @@ public class MovingSpinner extends View {
             case MotionEvent.ACTION_DOWN:
                 touchX = event.getX();
                 touchY = event.getY();
-                /*move = ((touchX < rectEndX && touchX > rectStartX) &&
-                        (touchY < rectEndY && touchY > rectStartY));*/
-//                move = region.contains((int) touchX, (int) touchY);
-//                move = false;
-                /*Path tempPath = new Path(); // Create temp Path
-                tempPath.moveTo(touchX,touchY); // Move cursor to point
-                RectF rectangle = new RectF(touchX - 1, touchY - 1, touchX + 1, touchY + 1); // create rectangle with size 2xp
-                tempPath.addRect(rectangle, Path.Direction.CW); // add rect to temp path
-                tempPath.op(shape.getPath(), Path.Op.DIFFERENCE); // get difference with our PathToCheck
-                move = !tempPath.isEmpty();*/
-//                move = shape.contains((int)touchX, (int)touchY);
                 move = shape.containsFine((int)touchX, (int)touchY);
-
                 break;
             case MotionEvent.ACTION_MOVE:
                 if (!move) {
@@ -108,14 +84,6 @@ public class MovingSpinner extends View {
                     invalidate();
                 }
                 break;
-            case MotionEvent.ACTION_UP:
-                // S04M04-3 lift up to move
-                /*float diffX = event.getX() - touchX;
-                float diffY = event.getY() - touchY;
-                x += diffX;
-                y += diffY;
-                invalidate();*/
-                break;
         }
         return true;
     }
@@ -127,86 +95,9 @@ public class MovingSpinner extends View {
      */
     @Override
     protected void onDraw(Canvas canvas) {
-        // S04M04-2 draw rectangle
-        final int width  = getWidth() / 2;
-        final int height = getHeight() / 2;
-
-        /*rectStartX = 100 + x;
-        rectStartY = 100 + y;
-        rectEndX = 100 + width + x;
-        rectEndY = 100 + height + y;*/
-
-//        shape.move((int)x, (int)y);
-//        canvas.translate(width, height);
-        canvas.drawPath(shape.getPath(), shape.getPaint());
-//        region.setPath(shape.getPath((int)x, (int)y, (int)rotation))
-
-//        path.moveTo(100, 100);
-//        path.lineTo(600, 100);
-//        path.lineTo(600, 600);
-//        path.lineTo(100, 600);
-//        path.close();
-//
-//        canvas.drawPath(path, paint);
-
-        // first 2 params are start x,y last two are end x,y
-        /*rectStartX = 100 + x;
-        rectStartY = 100 + y;
-        rectEndX = 100 + width + x;
-        rectEndY = 100 + height + y;
-
-        float rectCenterX = (rectEndX - rectStartX) / 2 + rectStartX;
-        float rectCenterY = (rectEndY - rectStartY) / 2 + rectStartY;
-
-//        rect.left = (int)rectStartX;
-//        rect.top = (int)rectStartY;
-//        rect.right = (int)rectEndX;
-//        rect.bottom = (int)rectEndY;
-
-//        float originRectStartX = rectStartX - rect.centerX();
-//        float originRectStartY = rectStartY - rect.centerY();
-//        float originRectEndX   = rectEndX - rect.centerX();
-//        float originRectEndY   = rectEndY - rect.centerY();
-
-//        rect.left = (int) originRectStartX;
-//        rect.top = (int) originRectStartY;
-//        rect.right = (int) originRectEndX;
-//        rect.bottom = (int) originRectEndY;
-
-        *//*rect.left = (int) ((originRectStartX * Math.cos(Math.toRadians(rotation))) - (originRectStartY * Math.sin(Math.toRadians(rotation))) + rect.centerX());
-        rect.top = (int) ((originRectStartX * Math.sin(Math.toRadians(rotation))) + (originRectStartY * Math.cos(Math.toRadians(rotation))) + rect.centerY());
-        rect.right = (int) ((originRectEndX * Math.cos(Math.toRadians(rotation))) - (originRectEndY * Math.sin(Math.toRadians(rotation))) + rect.centerX());
-        rect.bottom = (int) ((originRectEndX * Math.sin(Math.toRadians(rotation))) + (originRectEndY * Math.cos(Math.toRadians(rotation))) + rect.centerY());*//*
-
-        float originStartX = rectStartX - rectCenterX;
-        float originStartY = rectStartY - rectCenterY;
-        float originEndX   = rectEndX - rectCenterX;
-        float originEndY   = rectEndY - rectCenterY;
-
-        float startX = (float)((originStartX * Math.cos(Math.toRadians(rotation))) - (originStartY * Math.sin(Math.toRadians(rotation))) + rectCenterX);
-        float startY = (float)((originStartX * Math.sin(Math.toRadians(rotation))) + (originStartY * Math.cos(Math.toRadians(rotation))) + rectCenterY);
-        float endX = (float)((originEndX * Math.cos(Math.toRadians(rotation))) - (originEndY * Math.sin(Math.toRadians(rotation))) + rectCenterX);
-        float endY = (float)((originEndX * Math.sin(Math.toRadians(rotation))) + (originEndY * Math.cos(Math.toRadians(rotation))) + rectCenterY);
-
-        path.moveTo(startX, startY);
-        path.lineTo(endX, startY);
-        path.lineTo(endX, endY);
-        path.lineTo(startX, endY);
-        path.close();
-
-        canvas.drawPath(path, paint);*/
-
-//        canvas.save();
-//        canvas.rotate(rotation, rect.centerX(), rect.centerY());
-
-//        canvas.drawRect(rect, paint);
-//        canvas.restore();
-//        final Paint paint1 = new Paint();
-//        paint1.setColor(Color.MAGENTA);
-//        canvas.drawRect(rect, paint1);
-
-//        canvas.drawRect(rectStartX, rectStartY, rectEndX, rectEndY, paint);
-
+        if (shape != null) {
+            canvas.drawPath(shape.getPath(), shape.getPaint());
+        }
         super.onDraw(canvas);
     }
 }
